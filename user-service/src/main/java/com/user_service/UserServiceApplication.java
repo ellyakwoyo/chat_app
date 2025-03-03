34d7eea6911bb7910dba9af5ce739de8cb77bf63
Elly Opetu <ellyopetu@MacBookAir.stl-horizon.com>
2025-03-03 16:48:45 +0300

    Minor update

--- user-service/src/main/java/com/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/user_service/UserServiceApplication.java
@@ -14,7 +14,7 @@ public class UserServiceApplication {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+	public String hello(@RequestParam(value = "name", defaultValue = "Worlddd") String name) {
 	  return String.format("Hello %s!", name);
 	}
 
